edf423e34a355ee698204f4b3c8cba7e975911d2
godzy <isgodzy@msn.com>
2018-10-16 13:28:34 +0200

    Auto stash before merge of "master" and "origin/master"

--- kRHonos/src/app/app.module.ts
+++ kRHonos/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {AppComponent} from './app.component';
-import {SalarieListComponent} from './components/salarie/salarie-list/salarie-list.component';
-import {AppRoutingModule} from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { SalarieListComponent } from './components/salarie/salarie-list/salarie-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatPaginatorModule,
   MatTableModule,
@@ -25,20 +25,24 @@ import {
   MatFormFieldModule,
   MatInputModule,
   MatRadioModule,
-  MatStepperModule, MAT_DATE_FORMATS, DateAdapter, MAT_DATE_LOCALE,
+  MatStepperModule,
+  MatExpansionModule,
 } from '@angular/material';
-import {MainNavComponent} from './components/dashboard/main-nav/main-nav.component';
-import {LayoutModule} from '@angular/cdk/layout';
-import {MainDashComponent} from './components/dashboard/main-dash/main-dash.component';
-import {PlanningComponent} from './components/planning/planning.component';
-import {AbsencesComponent} from './components/absences/absences.component';
-import {AccueilComponent} from './components/accueil/accueil.component';
+import { MainNavComponent } from './components/dashboard/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MainDashComponent } from './components/dashboard/main-dash/main-dash.component';
+import { PlanningComponent } from './components/planning/planning.component';
+import { AbsencesComponent } from './components/absences/absences.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 import {SalarieFormComponent} from './components/salarie/salarie-form/salarie-form.component';
 import {SalarieTestComponent} from './components/salarie/salarie-test/salarie-test.component';
+import { ClockPickerDirective } from './modeles/clockpicker.directive';
+import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_LOCALE } from 'ng-pick-datetime';
 import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import { ContratComponent } from './components/contrat/contrat.component';
 import {PlanningDialogComponent} from './components/planning/planning-dialog/planning-dialog.component';
-import {ClockPickerDirective} from './modeles/clockpicker.directive';
-import {OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_LOCALE} from 'ng-pick-datetime';
+
 
 @NgModule({
   declarations: [
@@ -50,9 +54,10 @@ import {OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_LOCALE} from '
     PlanningComponent,
     AbsencesComponent,
     AccueilComponent,
-    SalarieTestComponent,
     PlanningDialogComponent,
-    ClockPickerDirective
+    ClockPickerDirective,
+    SalarieTestComponent,
+    ContratComponent,
   ],
   imports: [
     BrowserModule,
@@ -82,18 +87,18 @@ import {OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_LOCALE} from '
     MatDialogModule,
     MatCheckboxModule,
     MatDatepickerModule,
+    MatExpansionModule,
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
   ],
-  exports: [],
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: 'fr-FR'},
     {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
     {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
     {provide: OWL_DATE_TIME_LOCALE, useValue: 'fr'},
   ],
+  exports: [MatTableModule],
   entryComponents: [PlanningDialogComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
