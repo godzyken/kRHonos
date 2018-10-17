f8ce67e33b8fb2627ac9560638e7f74b6140e45e
CedricP <plouchard.cedric@gmail.com>
2018-10-17 16:48:49 +0200

    SidenavTest
    Sidenav test

--- kRHonos/src/app/components/dashboard/main-nav/main-nav.component.ts
+++ kRHonos/src/app/components/dashboard/main-nav/main-nav.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints, BreakpointState} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-components/dashboard/main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit {
+  sidenavWidth = 4;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
-    
-  constructor(private breakpointObserver: BreakpointObserver) {}
-  
+
+  constructor(private breakpointObserver: BreakpointObserver) {
   }
+
+  ngOnInit() {
+  }
+
+  increase() {
+    this.sidenavWidth = 15;
+    console.log("increase sidenav width");
+  }
+
+  decrease() {
+    this.sidenavWidth = 4;
+    console.log("decrease sidenav width");
+  }
+
+}
