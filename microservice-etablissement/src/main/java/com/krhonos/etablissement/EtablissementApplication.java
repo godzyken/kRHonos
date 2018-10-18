df0c5f5b1041b2ce6c394912f6455cfb936d55fe
CedricP <plouchard.cedric@gmail.com>
2018-10-18 13:50:42 +0200

    Microservice etablissement
    Microservice etablissement

---
+++ microservice-etablissement/src/main/java/com/krhonos/etablissement/EtablissementApplication.java
@@ -0,0 +1,12 @@
+package com.krhonos.etablissement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtablissementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtablissementApplication.class, args);
+	}
+}
