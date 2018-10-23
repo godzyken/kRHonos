a9bc0eb13bb6872c8a74381848b3d8744acb5381
godzy <isgodzy@msn.com>
2018-10-23 13:26:28 +0200

    petit commentaire

--- microservice-calendrier/src/main/java/com/krhonos/calendar/controller/PlanningController.java
+++ microservice-calendrier/src/main/java/com/krhonos/calendar/controller/PlanningController.java
@@ -11,6 +11,7 @@ import com.krhonos.calendar.repository.PlanningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
