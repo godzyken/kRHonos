9e77fd6dc6c4888166dd7d41fa94c1219a6ce609
godzyken <isgodzy@msn.com>
2018-10-24 14:09:22 +0200

    mise en place de la sécurité: eurekaServer
    Signed-off-by: godzy <isgodzy@msn.com>

--- microservice-contrat/src/main/java/com/krhonos/contrat/ContratApplication.java
+++ microservice-contrat/src/main/java/com/krhonos/contrat/ContratApplication.java
@@ -1,4 +1,4 @@
-package com.krhonos.etablissement;
+package com.krhonos.contrat;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,20 +6,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @EnableEurekaClient
 @SpringBootApplication
-public class EtablissementApplication {
+public class ContratApplication {
+
   /**
    * <b>OBJET QUI POSSEDE LES FONCTIONNALITES SUIVANTES : </b> <br/>
    * <br/>
    * Les fonctionnalites d'ecriture de messages de log dans la console.
    */
-  private static final Logger LOGGER = LoggerFactory.getLogger(EtablissementApplication.class);
-
+  private static final Logger LOGGER = LoggerFactory.getLogger(ContratApplication.class);
 
   public static void main(String[] args) {
-    LOGGER.info("CLASS : EtablissementApplication -- METHOD : main -- BEGIN");
-    SpringApplication.run(EtablissementApplication.class, args);
-    LOGGER.info("CLASS : EtablissementApplication -- METHOD : main -- END");
+    LOGGER.info("CLASS : ContratApplication -- METHOD : main -- BEGIN");
+    SpringApplication.run(ContratApplication.class, args);
+    LOGGER.info("CLASS : ContratApplication -- METHOD : main -- END");
   }
 }
