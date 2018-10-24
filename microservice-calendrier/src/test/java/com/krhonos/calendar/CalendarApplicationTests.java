17031fa267a7391eb81f5969bdbdfc780a033e19
godzyken <isgodzy@msn.com>
2018-10-24 15:51:01 +0200

    mise en place de la sécurité: eurekaServer + salaried + calendrier
    Signed-off-by: godzy <isgodzy@msn.com>

--- microservice-calendrier/src/test/java/com/krhonos/calendar/CalendarApplicationTests.java
+++ microservice-calendrier/src/test/java/com/krhonos/calendar/CalendarApplicationTests.java
@@ -2,6 +2,8 @@ package com.krhonos.calendar;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CalendarApplicationTests {
 
+  /**
+   * <b>OBJET QUI POSSEDE LES FONCTIONNALITES SUIVANTES : </b> <br/>
+   * <br/>
+   * Les fonctionnalites d'ecriture de messages de log dans la console.
+   */
+  private static final Logger LOGGER = LoggerFactory.getLogger(CalendarApplicationTests.class);
+
     @Test
     public void contextLoads() {
+      LOGGER.info("CLASS : CalendarApplicationTests -- METHOD : contextLoads -- BEGIN");
+      LOGGER.info("CLASS : CalendarApplicationTests -- METHOD : contextLoads -- END");
     }
 
 }
