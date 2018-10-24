8972a1c9a988d53dafc26b6ae7a293a07da32c98
guillaume_martin <guillaume.martin.jda@gmail.com>
2018-10-24 16:37:37 +0200

    microservice salarie
    maj de salarie, many to many en place entre utilisateur et role.

--- microservice-salarie/src/main/java/com/krhonos/salaried/controller/situationFamilialeController.java
+++ microservice-salarie/src/main/java/com/krhonos/salaried/controller/situationFamilialeController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
-public class situationFamilialeController {
+public class SituationFamilialeController {
     @Autowired
     SituationFamilialeDao repository;
 
--- microservice-salarie/src/main/java/com/krhonos/salaried/model/Candidat.java
+++ microservice-salarie/src/main/java/com/krhonos/salaried/model/Candidat.java
@@ -9,4 +9,21 @@ public class Candidat extends Personne {
 
     @Column(name = "candidat_date_dispo")
     private Date date_dispo;
+
+    public Candidat() {
+        super();
+    }
+
+    public Candidat(String nom, String nomNaissance, String prenom, Date date_dispo) {
+        super(nom, nomNaissance, prenom);
+        this.date_dispo = date_dispo;
+    }
+
+    public Date getDate_dispo() {
+        return date_dispo;
+    }
+
+    public void setDate_dispo(Date date_dispo) {
+        this.date_dispo = date_dispo;
+    }
 }
