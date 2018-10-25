3d4a2b4c25a20c6ba4d6a59671f9acba4645bbbd
CedricP <plouchard.cedric@gmail.com>
2018-10-25 16:53:35 +0200

    Paramétrage Zuul
    Zuul

--- zuul_server/src/test/java/com/krhonos/zuul_server/ZuulServerApplicationTests.java
+++ zuul_server/src/test/java/com/krhonos/zuul_server/ZuulServerApplicationTests.java
@@ -2,6 +2,8 @@ package com.krhonos.zuul_server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,6 +11,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ZuulServerApplicationTests {
 
+  /**
+   * <b>OBJET QUI POSSEDE LES FONCTIONNALITES SUIVANTES : </b> <br/>
+   * <br/>
+   * Les fonctionnalites d'ecriture de messages de log dans la console.
+   */
+  private static final Logger LOGGER = LoggerFactory.getLogger(ZuulServerApplicationTests.class);
+
   @Test
-  public void contextLoads() {}
+  public void contextLoads() {
+    LOGGER.info("CLASS : ZuulServerApplicationTests -- METHOD : contextLoads -- BEGIN");
+    LOGGER.info("CLASS : ZuulServerApplicationTests -- METHOD : contextLoads -- END");
+  }
 }
