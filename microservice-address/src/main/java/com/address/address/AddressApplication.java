413bcbee0ca3ff2202480dab632aacdf51beb6a3
Gaspi02 <duringer.gaspard@gmail.com>
2018-10-26 11:44:20 +0200

    extarnalisation config adress

--- microservice-address/src/main/java/com/address/address/AddressApplication.java
+++ microservice-address/src/main/java/com/address/address/AddressApplication.java
@@ -3,8 +3,10 @@ package com.address.address;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigurationProperties
+@EnableEurekaClient
 @SpringBootApplication
 public class AddressApplication {
 
