0e20607c91406f72ca0800b4c190b6c0c22364c6
Gaspi02 <duringer.gaspard@gmail.com>
2018-10-26 11:17:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- microservice-etablissement/src/main/java/com/krhonos/etablissement/EtablissementApplication.java
+++ microservice-etablissement/src/main/java/com/krhonos/etablissement/EtablissementApplication.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigurationProperties
 @EnableEurekaClient
 @SpringBootApplication
 public class EtablissementApplication {
