25e0ee189c68f0968915d83f685ae85059ad3f4d
Gaspi02 <duringer.gaspard@gmail.com>
2018-10-26 11:17:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- microservice-contrat/src/main/java/com/krhonos/contrat/ContratApplication.java
+++ microservice-contrat/src/main/java/com/krhonos/contrat/ContratApplication.java
@@ -1,12 +1,28 @@
 package com.krhonos.contrat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableConfigurationProperties
+@EnableEurekaClient
 @SpringBootApplication
 public class ContratApplication {
 
+  /**
+   * <b>OBJET QUI POSSEDE LES FONCTIONNALITES SUIVANTES : </b> <br/>
+   * <br/>
+   * Les fonctionnalites d'ecriture de messages de log dans la console.
+   */
+  private static final Logger LOGGER = LoggerFactory.getLogger(ContratApplication.class);
+
   public static void main(String[] args) {
+    LOGGER.info("CLASS : ContratApplication -- METHOD : main -- BEGIN");
     SpringApplication.run(ContratApplication.class, args);
+    LOGGER.info("CLASS : ContratApplication -- METHOD : main -- END");
   }
 }
